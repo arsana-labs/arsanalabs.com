4671adf860ce3cd9c965968e11a7408594fb9075
GopalVerma1303 <vgopaldps@gmail.com>
2024-12-25 23:11:31 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import { Github } from "lucide-react";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-screen w-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <Image
+          className="dark:invert"
+          src="/gnm-crop-light.png"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+        <ol className="list-inside text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+          <li className="mb-2">We build open-source tools.</li>
+          <li>We provide expert software consulting.</li>
+        </ol>
+
+        <div className="flex gap-4 items-center flex-col sm:flex-row">
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="https://github.com/gnmlabs"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Github size={20} />
+            GitHub
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            href="https://github.com/orgs/gnmlabs/people"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Visit our team
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}

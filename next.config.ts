36358de9cae6b495cec9d39d78dc9819462d0cf1
GopalVerma1303 <vgopaldps@gmail.com>
2024-12-25 23:26:45 +0530

    updated next config for local dev and readme

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/gnmlabs",
-  assetPrefix: "/gnmlabs/",
+  basePath: isProduction ? "/gnmlabs" : "",
+  assetPrefix: isProduction ? "/gnmlabs" : "",
   images: {
     unoptimized: true,
   },

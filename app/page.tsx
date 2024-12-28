22fefcb05432d3f231f2eaf2b4af312ed373e305
GopalVerma1303 <vgopaldps@gmail.com>
2024-12-28 22:57:29 +0530

    updated: home links added view project and book-a-call btn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import { Github } from "lucide-react";
+import { Github, Folders, Calendar } from "lucide-react";
 
 export default function Home() {
   return (
@@ -17,7 +17,6 @@ export default function Home() {
           <li className="mb-2">We build open-source products.</li>
           <li>We provide expert software consulting.</li>
         </ol>
-
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
@@ -29,12 +28,22 @@ export default function Home() {
             GitHub
           </a>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://github.com/orgs/arsana-labs/people"
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 gap-2"
+            href="https://github.com/orgs/arsana-labs/repositories"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Visit our team
+            <Folders size={20} />
+            View our projects
+          </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44 gap-2"
+            href="https://cal.com/bettercallgopal/personalizedcall"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Calendar size={20} />
+            Book a Call
           </a>
         </div>
       </main>
